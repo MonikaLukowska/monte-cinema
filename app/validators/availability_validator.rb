50a62eef57f8fbcee847c36ce53707fab6550080
Monika Łukowska <monika.lukowska@gmail.com>
2022-09-20 10:29:20 +0200

    fix:exclude record from validator date ranges
    Including validated record in seances collection causes datetime conflict and validation failure

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,6 +1,6 @@
 class AvailabilityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    seances = Seance.includes(:movie).where(hall: record.hall)
+    seances = Seance.includes(:movie).where(hall: record.hall).where.not(id: record.id)
     date_ranges = seances.map do |seance|
       seance.start_time..(seance.start_time + seance.movie_length.minutes + ADS.minutes)
     end
