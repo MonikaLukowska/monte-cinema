c17e4334aba6f2b93fd5a58d134c623457761308
Monika Łukowska <monika.lukowska@gmail.com>
2022-09-26 14:03:15 +0200

    ref:adjust validator parameter name

--- app/validators/seat_availability_validator.rb
+++ app/validators/seat_availability_validator.rb
@@ -1,10 +1,10 @@
 class SeatAvailabilityValidator < ActiveModel::Validator
-  def validate(record)
-    seat = record.seat
-    seat_hash = record.seance.seats_availability.detect { |hash| hash[:seat] == seat }
+  def validate(ticket)
+    seat = ticket.seat
+    seat_hash = ticket.seance.seats_availability.detect { |hash| hash[:seat] == seat }
     status = seat_hash[:status] if seat_hash
     return if status == 'available'
 
-    record.errors.add(:seat, "#{seat} is not available")
+    ticket.errors.add(:seat, "#{seat} is not available")
   end
 end
