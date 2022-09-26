599743e8403dd7d9ebf8ab387e753ad8db1798bb
Monika Łukowska <monika.lukowska@gmail.com>
2022-09-26 17:54:12 +0200

    feat:add status to reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,11 @@
 require 'uri'
 
 class Reservation < ApplicationRecord
+  CREATED = 'created'.freeze
+  CONFIRMED = 'confirmed'.freeze
+  CANCELLED = 'cancelled'.freeze
+  RESERVATION_STATUSES = [CREATED, CONFIRMED, CANCELLED].freeze
+
   belongs_to :seance
   has_many :tickets, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_093216) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_123726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_093216) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "created", null: false
     t.index ["seance_id"], name: "index_reservations_on_seance_id"
   end
 
