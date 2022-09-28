6d3781a4e13ed5a589fa64eff1113769ff915bbc
MonikaLukowska <56922072+MonikaLukowska@users.noreply.github.com>
2022-09-28 11:18:16 +0200

    Feature/add users (#12)
    * chore:add devise
    
    * feat:add user
    
    * feat:add views for authentication
    
    * test:add model spec for password length
    
    * feat:add user seeds
    
    * test:refactor according CR remarks
    
    change tested password length to correspond with validator; add aggregate_failure flag

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 #frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
  
- resources :movies, except: [:show]
- resources :seances do
-  resources :reservations, only: %i[new create]
- end
+  resources :movies, except: [:show]
+  resources :seances do
+    resources :reservations, only: %i[new create]
+  end
  
- root to: 'seances#index'
+  root to: 'seances#index'
 end
