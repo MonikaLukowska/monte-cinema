668e1de4245e556cda6f6383feab713c66164736
Monika Łukowska <monika.lukowska@gmail.com>
2022-09-29 14:51:01 +0200

    test:update reservation specs

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -7,19 +7,6 @@ RSpec.describe Reservation, type: :model do
 
   describe 'associations' do
     it { is_expected.to belong_to(:seance) }
-  end
-
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:email) }
-
-    it 'is invalid with wrong e-mail format' do
-      reservation = build(:reservation, email: 'example.com')
-      expect(reservation).not_to be_valid
-    end
-
-    it 'is valid with correct email format' do
-      reservation = build(:reservation, email: 'example@example.com')
-      expect(reservation).to be_valid
-    end
+    it { is_expected.to belong_to(:user) }
   end
 end
