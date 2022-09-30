f7484eccf2c91bb58f9fd0868fa80ce902b0f65d
Monika Łukowska <monika.lukowska@gmail.com>
2022-09-30 16:45:25 +0200

    feat:create routes for cancel and confirm reservation

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@
 
 Rails.application.routes.draw do
   devise_for :users
-
- resources :movies, except: [:show]
- resources :seances do
-  resources :reservations, except: %i[destroy]
- end
+  resources :movies, except: [:show]
+  
+  resources :seances do
+    resources :reservations, only: %i[new create index]
+  end
+  resources :reservations, only: %i[index] do
+    member do
+    patch 'cancel', to: 'reservations#cancel'
+    patch 'confirm', to: 'reservations#confirm'
+    end
+  end
  
   root to: 'seances#index'
 end
