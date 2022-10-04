444ebc86bcafe434d4c80e973602742101d5163c
Monika Łukowska <monika.lukowska@gmail.com>
2022-10-04 16:19:18 +0200

    feat:add sidekiq, sidekiq cron and redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 #frozen_string_literal: true
 
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+  
   devise_for :users
   resources :movies, except: [:show]
   
