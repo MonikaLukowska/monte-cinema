3fd134f31363a57fd85cd9543e3752f9b4a24f59
Monika Łukowska <monika.lukowska@gmail.com>
2022-10-07 20:44:00 +0200

    refactor: change DateTime.now to DateTime.current

--- spec/factories/seances.rb
+++ spec/factories/seances.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :seance do
     association :movie
     association :hall
-    start_time { DateTime.now }
+    start_time { DateTime.current }
     price { 15 }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "testy#{n}@example.com" }
     password { 'password123' }
-    confirmed_at { DateTime.now }
+    confirmed_at { DateTime.current }
 
     factory :manager do
       role { :manager }
