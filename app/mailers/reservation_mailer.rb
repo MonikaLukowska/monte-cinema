c6ee134cc520afe813f26dc16a8b4378a0d70baa
Monika Łukowska <monika.lukowska@gmail.com>
2022-10-10 22:21:01 +0200

    feat:add reservation confirmation mailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -7,4 +7,13 @@ class ReservationMailer < ApplicationMailer
       subject: 'Your seance reservation has been cancelled'
     )
   end
+
+  def confirm_reservation_email(reservation)
+    @reservation = reservation
+
+    mail(
+      to: @reservation.user.email,
+      subject: 'Your successfully created seance reservation'
+    )
+  end
 end
