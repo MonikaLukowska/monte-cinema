85ab87a430b309275ae239a7d932c5cdac6053dc
MonikaLukowska <56922072+MonikaLukowska@users.noreply.github.com>
2022-10-12 21:21:53 +0200

    fix:use overlaps instead covers (#22)

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,11 +1,16 @@
 class AvailabilityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     seances = Seance.includes(:movie).where(hall: record.hall).where.not(id: record.id)
+
+    return unless !seances.empty? && record && value
+
+    record_range = value..(value + record.movie_length.minutes + ADS.minutes)
     date_ranges = seances.map do |seance|
       seance.start_time..(seance.start_time + seance.movie_length.minutes + ADS.minutes)
     end
+
     date_ranges.each do |range|
-      record.errors.add(attribute, 'not available') if range.cover? value
+      record.errors.add(attribute, 'not available') if range.overlaps? record_range
     end
   end
 end
