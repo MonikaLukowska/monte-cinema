a92d9aebb6fcde77cbbb0edfd6284ee68121f458
Monika Łukowska <monika.lukowska@gmail.com>
2022-10-12 09:57:51 +0200

    feat:add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   mount Sidekiq::Web => '/sidekiq'
   
   devise_for :users
@@ -21,4 +23,15 @@ Rails.application.routes.draw do
   end
  
   root to: 'seances#index'
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :movies, only: [:index] do
+        collection do
+          get 'find_by_date', as: 'screening_on'
+        end
+      end
+      resources :seances, only: [:show]
+    end
+  end
 end
