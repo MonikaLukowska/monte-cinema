3e2bd4f555a7393b2acab6c30c71b5db62ca6fcb
Monika Łukowska <monika.lukowska@gmail.com>
2022-10-12 11:03:29 +0200

    refactor:rescue from not found globally

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class ApiController < ActionController::API
+      rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+      private
+
       def render_not_found
         render json: { error: 'Record not found', code: 404, status: 'error' }, status: :not_found
       end
