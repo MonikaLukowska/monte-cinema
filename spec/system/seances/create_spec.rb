3ad216dd0a44e638cb652262c37534fec7d596fb
Monika Łukowska <monika.lukowska@gmail.com>
2022-10-12 21:19:30 +0200

    fix:use overlaps instead covers

--- spec/system/seances/create_spec.rb
+++ spec/system/seances/create_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Seance management', type: :system do
   end
 
   context 'when creating correct seance' do
-    let(:date) { DateTime.current }
+    let(:date) { DateTime.current + 4.hours }
     let(:formatted_date) { date.strftime('%Y-%m-%dT%H:%M') }
 
     it 'creates a new seance' do
@@ -19,13 +19,13 @@ RSpec.describe 'Seance management', type: :system do
       fill_in('Start time', with: formatted_date)
       click_button('Create Seance')
       expect(page).to have_content('Seance was successfully added')
-      expect(page).to have_content(seance.start_time.strftime('%H:%M'))
+      expect(page).to have_content(date.strftime('%H:%M'))
     end
   end
 
   context 'when creating invalid seance' do
-    let(:date2) { 1.hour.from_now }
-    let(:formatted_date2) { date2.strftime('%Y-%m-%dT%H:%M') }
+    let(:date) { DateTime.current }
+    let(:formatted_date) { date.strftime('%Y-%m-%dT%H:%M') }
 
     it 'shows an error when price is not present' do
       click_link('New seance')
@@ -36,7 +36,7 @@ RSpec.describe 'Seance management', type: :system do
     it 'shows an error when new seance time covers another seance time in chosen hall' do
       click_link('New seance')
       select(seance.hall.name, from: 'Hall')
-      fill_in('Start time', with: formatted_date2)
+      fill_in('Start time', with: formatted_date)
       fill_in('Price', with: 15)
       click_button('Create Seance')
       expect(page).to have_text('Start time not available')
